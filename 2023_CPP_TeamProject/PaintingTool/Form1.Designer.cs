48dc199ba5ea658360c26ef2c4f2ee5385897183
MCGom <100755492+MCGom@users.noreply.github.com>
2023-11-29 01:06:25 +0900

    제한 시간 프로그레스 바에서 시간초로 변경

---
+++ 2023_CPP_TeamProject/PaintingTool/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace PaintingTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.GameStart = new System.Windows.Forms.Button();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            this.TimeLeft = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // GameStart
+            // 
+            this.GameStart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.GameStart.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.GameStart.Font = new System.Drawing.Font("Elephant", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GameStart.ForeColor = System.Drawing.Color.DarkGreen;
+            this.GameStart.Location = new System.Drawing.Point(200, 300);
+            this.GameStart.Name = "GameStart";
+            this.GameStart.Size = new System.Drawing.Size(200, 100);
+            this.GameStart.TabIndex = 0;
+            this.GameStart.Text = "게임 시작";
+            this.GameStart.UseVisualStyleBackColor = false;
+            this.GameStart.Click += new System.EventHandler(this.GameStart_Click);
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Tick += new System.EventHandler(this.GameTimer_Tick);
+            // 
+            // TimeLeft
+            // 
+            this.TimeLeft.BackColor = System.Drawing.Color.Sienna;
+            this.TimeLeft.Font = new System.Drawing.Font("Elephant", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TimeLeft.ForeColor = System.Drawing.Color.Yellow;
+            this.TimeLeft.Location = new System.Drawing.Point(200, 11);
+            this.TimeLeft.Name = "TimeLeft";
+            this.TimeLeft.Size = new System.Drawing.Size(200, 50);
+            this.TimeLeft.TabIndex = 1;
+            this.TimeLeft.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(580, 757);
+            this.Controls.Add(this.TimeLeft);
+            this.Controls.Add(this.GameStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SimpleGame";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button GameStart;
+        private System.Windows.Forms.Timer GameTimer;
+        private System.Windows.Forms.Label TimeLeft;
+    }
+}
+
---
+++ 2023_CPP_TeamProject/PaintingTool/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Windows.Forms.VisualStyles;
+
+namespace PaintingTool
+{
+
+    public partial class Form1 : Form
+    {
+        private Rectangle Ground;
+        private Rectangle Player;
+        private Rectangle[] Woods = new Rectangle[3];
+
+        private Brush Player_B;
+        private Brush Woods_B;
+
+
+        private Boolean GameStarted = false;
+        private Boolean IsLeftClicked = true;
+
+        private Random WoodType;
+
+        private int NowWood = 0;
+        private int Score = 0;
+        private int TimeLimit = 300;
+        private int TimerTime = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            DoubleBuffered = true;
+
+            Size = new Size(600, 800);
+
+            Ground = new Rectangle(0, 600, 600, 200);
+
+            Player_B = new SolidBrush(Color.Orange);
+            Woods_B = new SolidBrush(Color.SaddleBrown);
+
+            WoodType = new Random();
+            this.TimeLeft.Hide();
+
+
+
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Left)
+            {
+                if (IsLeftClicked == false)
+                {
+                    Player.Offset(-450, 0);
+                    IsLeftClicked = true;
+                }
+                WoodChanged(NowWood);
+                this.Invalidate();
+                
+                WoodNumbering();
+                GameOver(Player, Woods[NowWood]);
+                TimeControl();
+
+
+            }
+            else if(e.KeyCode == Keys.Right)
+            {
+                if (IsLeftClicked == true)
+                {
+                    Player.Offset(450, 0);
+                    IsLeftClicked = false;
+                }
+                WoodChanged(NowWood);
+                this.Invalidate();
+                
+                WoodNumbering();
+                GameOver(Player, Woods[NowWood]);
+                TimeControl();
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {           
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Brush b = new SolidBrush(Color.Green);
+
+            g.FillRectangle(b, Ground);
+            if(GameStarted == true)
+            {
+                for(int i =0; i < 3; i++)
+                {
+                    g.FillRectangle(Woods_B, Woods[i]);
+                }
+                g.FillRectangle(Player_B, Player);
+            }
+
+        }
+
+        private void GameStart_Click(object sender, EventArgs e)
+        {
+            this.GameStart.Hide();
+
+            GameStarted = true;
+
+            IsLeftClicked = true;
+
+            Woods[0] = new Rectangle(190, 400, 370, 200);
+            Woods[1] = new Rectangle(20, 200, 370, 200);
+            Woods[2] = new Rectangle(190, 0, 370, 200);
+
+            Player = new Rectangle(50, 500, 50, 100);
+            this.TimeLeft.Show();
+            this.Invalidate();
+            this.Focus();
+
+            TimeLimit = 300;
+            TimeControl();
+           
+        }
+        private void GameTimer_Tick(object sender, EventArgs e)
+        {
+            if(TimerTime <= 0)
+            {
+                GameTimer.Stop();
+                MessageBox.Show("Score : " + Score, "게임 오버", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Score = 0;
+                GameStarted = false;
+                NowWood = 0;
+                this.Invalidate();
+                this.GameStart.Show();
+                this.TimeLeft.Hide();
+                TimerTime = 0;
+            }
+            else
+            {
+                TimeLeft.Text = TimerTime / 10 + "." + TimerTime % 10;
+                TimerTime--;
+            }
+        }
+
+        private void WoodChanged(int idx)
+        {
+            int WoodTypeTmp = WoodType.Next(2);
+
+            switch(WoodTypeTmp)
+            {
+                case 0:
+                    {
+                        Woods[idx].Location = new Point(190, 0);
+                        WoodIndexing(idx);
+                    }
+                    break;
+                case 1:
+                    {
+                        Woods[idx].Location = new Point(20, 0);
+                        WoodIndexing(idx);
+                    }
+                    break;
+            }
+        }
+        private void WoodIndexing(int idx)
+        {
+            if (idx == 2)
+            {
+                Woods[0].Location = new Point(Woods[0].X, 400);
+                Woods[1].Location = new Point(Woods[1].X, 200);
+            }
+            else if (idx == 1)
+            {
+                Woods[2].Location = new Point(Woods[2].X, 400);
+                Woods[0].Location = new Point(Woods[0].X, 200);
+            }
+            else
+            {
+                Woods[1].Location = new Point(Woods[1].X, 400);
+                Woods[2].Location = new Point(Woods[2].X, 200);
+            }
+        }
+
+        private void WoodNumbering()
+        {
+            if(NowWood != 2)
+            {
+                NowWood++;
+            }
+            else
+            {
+                NowWood = 0;
+            }
+        }
+
+        private void GameOver(Rectangle player, Rectangle wood)
+        {
+            Rectangle isIntersect = Rectangle.Intersect(player, wood);
+
+            if (!isIntersect.IsEmpty)
+            {
+                GameTimer.Stop();
+                MessageBox.Show("Score : " + Score, "게임 오버", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Score = 0;
+                GameStarted = false;
+                NowWood = 0;
+                this.Invalidate();
+                this.GameStart.Show();
+                this.TimeLeft.Hide();
+                TimerTime = 0;
+            }
+            else
+            {
+                Score++;
+            }
+        }
+
+        private void TimeControl()
+        {
+            GameTimer.Stop();
+            if (TimeLimit > 10)
+            {
+                TimeLimit -= 5;
+            }
+            TimerTime = TimeLimit;
+            TimeLeft.Text = TimerTime / 10  + "." + TimerTime % 10;
+            GameTimer.Start();
+        }
+    }
+}
